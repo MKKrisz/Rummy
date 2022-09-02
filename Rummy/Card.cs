bf53cb1f490708dcebc4464a472c411044f87216
MKKrisz <magyari.krisztian@szilagyi-eger.hu>
2022-09-02 10:34:29 +0200

    added back value enum for easier value checks added function: Card.Copy     creates a copy of the card for value-based input

--- Rummy/Card.cs
+++ Rummy/Card.cs
@@ -1,13 +1,20 @@
 namespace Rummy
 {
     public enum Suit{Spades = 0, Hearts = 1, Diamonds = 3, Clubs = 2}
+    public enum Value{Joker, Ace, N2, N3, N4, N5, N6, N7, N8, N9, N10, Jack, Queen, King}
+    
     public class Card
     {
-        public Suit Suit;
+        public Suit Suit;   
         public int Value;
 
         public int PointValue => GetValue();
 
+        public Card Copy()
+        {
+            Card cp = (Card)this.MemberwiseClone();
+            return cp;
+        }
         private int GetValue()
         {
             if (Value >  10) { return 10; }
