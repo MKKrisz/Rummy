60548a440d30c9b42d51715f23837812dd423900
MKKrisz <magyari.krisztian@szilagyi-eger.hu>
2022-09-02 10:32:36 +0200

    minor improvements (Ace is now A)

--- Rummy/Program.cs
+++ Rummy/Program.cs
@@ -4,7 +4,7 @@ namespace Rummy
 {
     class Program
     {
-        public static string[] Value = { "Joker", "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+        public static string[] Value = { "Joker", "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
         public static Random r;
 
         public static Game Game;
