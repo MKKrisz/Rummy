e274852e02ba45b3f5f847881931a72dac8f20f2
MKKrisz <magyari.krisztian@szilagyi-eger.hu>
2022-09-08 11:27:08 +0200

    changed default behavior of method "Sort" refactored most outputs to now use TextColor instead of native Ansi made outputs look more colorful added AutoCompleteParameter attribute for where it's needed

--- Rummy/Hand.cs
+++ Rummy/Hand.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using TextColor;
 
 namespace Rummy
 {
@@ -22,7 +23,7 @@ namespace Rummy
         
         public enum SortType{Suit = 0, Value = 1, Both = 2}
         [PlayerInvokable(Name = "Sort", Description = "Sorts the hand based on input (0/Suit, 1/Value, 2/Both)")]
-        public void Sort(SortType T = SortType.Value)
+        public void Sort(SortType T = SortType.Both)
         {
             Card[] buffer = Cards.ToArray();
             if (T == SortType.Suit || T == SortType.Both)
@@ -61,12 +62,12 @@ namespace Rummy
         {
             if (a > Cards.Count || b > Cards.Count)
             {
-                Console.Write("Error: one index is out of range");
+                Console.WriteLine($"{Colors.Error.AnsiFGCode}[ERROR]: one index is out of range{Color.Reset}");
                 return;
             }
             if (a == b)
             {
-                Console.Write("Error: the ID-s are the same, switching is not required");
+                Console.WriteLine($"{Colors.Warning.AnsiFGCode}[WARNING]: the ID-s are the same, switching is not required{Color.Reset}");
                 return;
             }
 
@@ -77,7 +78,7 @@ namespace Rummy
         
         [PlayerInvokable(Name = "Discard", Description = "Discards a card, and thus ends the turn")]
         [TurnEnder]
-        public void Discard(List<Card> DiscardPile, int id)
+        public void Discard([AutoCompleteParameter]List<Card> DiscardPile, int id)
         {
             DiscardPile.Add(Cards[id]);
             Cards.RemoveAt(id);
@@ -88,17 +89,16 @@ namespace Rummy
         {
             for (int i = 0; i < Cards.Count; i++)
             {
-                int[] BG = { 0, 0, 0 };
+                string BG = Color.Reset;
                 
                 bool Selected = Selection.IndexOf(i) != -1; 
-                if(Selected){BG = new int[] {0, 128, 128};}
+                if(Selected){BG = Colors.Selected.AnsiBGCode;}
                 
-                if(!horizontal) Console.WriteLine($"{i}:\t\u001b[48;2;{BG[0]};{BG[1]};{BG[2]}m{Program.Suit[(int)Cards[i].Suit]}\u001b[48;2;{BG[0]};{BG[1]};{BG[2]}m{Program.Value[Cards[i].Value]}\u001b[m");
+                if(!horizontal) Console.WriteLine($"{i}:\t{BG}{Program.Suit[(int)Cards[i].Suit]}{BG}{Program.Value[Cards[i].Value]}{Color.Reset}");
                 if (horizontal)
                 {
-                    Console.Write($"\u001b[48;2;{BG[0]};{BG[1]};{BG[2]}m{Program.Suit[(int)Cards[i].Suit]}\u001b[48;2;{BG[0]};{BG[1]};{BG[2]}m{Program.Value[Cards[i].Value]}\u001b[m ");
+                    Console.Write($"{BG}{Program.Suit[(int)Cards[i].Suit]}{BG}{Program.Value[Cards[i].Value]}{Color.Reset} ");
                 }
-                if(Selected){Console.ResetColor();}
             }
             if(horizontal){Console.Write("\n");}
         }
@@ -114,12 +114,12 @@ namespace Rummy
         }
 
         [PlayerInvokable(Name = "Meld",  Description = "Creates a meld made of the selection given")]
-        public void Meld(List<Meld> melds, params object[] selection)
+        public void Meld([AutoCompleteParameter]List<Meld> melds, params object[] selection)
         {
             if (selection.Length == 0)
             {
-                if(Selection.Count == 0){Console.Write("Nothing was selected, no melds were created");return;}
-                else if(Selection.Count<3){Console.Write("Less than the required amount of cards were selected, no melds were created");return;}
+                if(Selection.Count == 0){Console.WriteLine($"{Colors.Error.AnsiFGCode}[ERROR]: Nothing was selected, no melds were created{Color.Reset}");return;}
+                if(Selection.Count  < 3){Console.WriteLine($"{Colors.Error.AnsiFGCode}[ERROR]: Less than the required amount of cards were selected, no melds were created{Color.Reset}");return;}
             }
             else
             {
@@ -134,7 +134,7 @@ namespace Rummy
             {
                 newMeld = Rummy.Meld.Melder(SelectedCards);
             }
-            catch(Exception E) { Console.WriteLine(E.Message); success = false;}
+            catch(Exception E) { Console.WriteLine($"{Colors.Error.AnsiFGCode}[ERROR]: {E.Message}"); success = false;}
 
             if (success)
             {
