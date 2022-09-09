b9edb7b1d46cf5f2e4a0c0d66650542d70991b11
MKKrisz <magyari.krisztian@szilagyi-eger.hu>
2022-09-09 18:53:38 +0200

    Created the first version of the game loop. It still lacks a ton of features that need implementing down the line

--- Rummy/Game.cs
+++ Rummy/Game.cs
@@ -12,6 +12,7 @@ namespace Rummy
         public List<Meld> Melds = new List<Meld>();
 
         public int Round;
+        public bool Run = true;
 
         public Game(int PlayerAmount)
         {
@@ -28,7 +29,25 @@ namespace Rummy
 
         public void Loop()
         {
-            Players[0].SH.StartTurn();
+            int i = 0;
+            while (Run)
+            {
+                if (i == Players.Length) {i = 0;Round++;}
+                if ((i != 0 || Round != 0) && Deck.CardsLeft != 0) {Players[i].Hand.Cards.Add(Deck.Draw(Program.r));}
+                else if(Deck.CardsLeft == 0 && TrumpCard != null){Players[i].Hand.Cards.Add(TrumpCard.Copy()); TrumpCard = null;}
+                if (Deck.CardsLeft == 0) {
+                    Deck.AddCards(DiscardPile);
+                    DiscardPile.Clear();
+                }
+                Players[i].SH.StartTurn();
+                if (Players[i].Hand.Cards.Count == 0)
+                {
+                    Run = false;
+                }
+
+
+                i++;
+            }
         }
     }
 }
